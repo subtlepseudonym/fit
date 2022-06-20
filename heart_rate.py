52d1e8213be9c2385a896adf71fc307c2993395c
Connor <subtlepseudonym@gmail.com>
2022-06-20 14:54:50 -0400

    Add support for tracking activity

--- heart_rate.py
+++ heart_rate.py
@@ -1,21 +1,43 @@
 import json
 import sys
-import numpy as np
+import time
 
 from fitparse import FitFile
 from datetime import datetime
-from time import mktime
+
+
+file_type_monitoring = 'monitoring_b'
+file_type_activity = 'activity'
+file_type_tracking = 'tracking'
+tracking_activity_name = 'All-Day Tracking'
 
 
 def get_file_type(messages) -> str:
     for msg in messages:
-        if msg.name == 'file_id' and msg.get('type'):
+        if msg.name == 'file_id' and msg.get_value('type'):
             return msg.get_value('type')
 
 
-def get_base_timestamp(messages) -> datetime:
+# is_tracking_activity is used to modify file type in the case
+# that the data represents an activity exclusively for tracking
+def is_tracking_activity(messages) -> bool:
     for msg in messages:
-        if msg.name == 'device_info' and msg.get('timestamp'):
+        if msg.name == 'sport' and msg.get_value('name'):
+            return msg.get_value('name') == tracking_activity_name
+
+
+def get_base_timestamp(file_type, messages) -> datetime:
+    if file_type == file_type_activity:
+        name = 'device_info'
+        field = 'timestamp'
+    elif file_type == file_type_monitoring:
+        name = 'monitoring_info'
+        field = 'local_timestamp'
+    else:
+        return time.gmtime(0)
+
+    for msg in messages:
+        if msg.name == name and msg.get_value(field):
             return msg.get_value('timestamp')
 
 
@@ -24,35 +46,32 @@ def activity_timestamp(base_ts, msg) -> datetime:
 
 
 def monitoring_timestamp(base_ts, msg) -> datetime:
-    base = np.uintc(mktime(base_ts.timetuple()))
+    base = np.uintc(time.mktime(base_ts.timetuple()))
     ts16 = np.ushort(msg.get_value('timestamp_16'))
     ts = base + ((ts16 - (base & 0xFFFF)) & 0xFFFF) + 19800
     return datetime.utcfromtimestamp(ts)
-    #sum = mktime(base_ts.timetuple()) + msg.get_value('timestamp_16')
+    #sum = time.mktime(base_ts.timetuple()) + msg.get_value('timestamp_16')
     #mesgTimestamp += ( timestamp_16 - ( mesgTimestamp & 0xFFFF ) ) & 0xFFFF
     #return datetime.utcfromtimestamp(sum)
 
 
 def get_heart_rate(messages) -> list:
     file_type = get_file_type(messages)
-    base_ts = get_base_timestamp(messages)
+    base_ts = get_base_timestamp(file_type, messages)
 
     records = []
     for msg in messages:
-        if msg.get('heart_rate') is None:
-            continue
-
         heart_rate = msg.get_value('heart_rate')
-        if heart_rate <= 0:
+        if heart_rate is None or heart_rate <= 0:
             continue
 
-        if file_type == 'activity':
+        if file_type == file_type_activity:
             ts = activity_timestamp(base_ts, msg)
-        elif file_type == 'monitoring_b':
+        elif file_type == file_type_monitoring:
             ts = monitoring_timestamp(base_ts, msg)
 
         #timestamp = ts  # datetime
-        timestamp = mktime(ts.timetuple())  # unix int
+        timestamp = time.mktime(ts.timetuple())  # unix int
         #timestamp = ts.strftime('%Y-%m-%dT%H:%M:%SZ')
         records.append({'timestamp': timestamp, 'heart_rate': msg.get_value('heart_rate')})
 
--- plot_heart_rate.py
+++ plot_heart_rate.py
@@ -7,36 +7,54 @@ import matplotlib.dates as mdates
 
 from fitparse import FitFile
 
-hrr = []
+
+def clean_data(data) -> list:
+    data = np.array(data)
+    ts = data[:,0]
+    bpm = data[:,1]
+
+    # Filter large breaks in data continuity
+    dx = ts[1:] - ts[:-1]
+    x = np.insert(ts, np.where(dx>60)[0]+1, np.nan)
+    x = np.asarray(x, dtype='datetime64[s]')
+    x = list(map(lambda a: a - np.timedelta64(9, 'h'), x))  # UTC to CST
+    y = np.insert(bpm, np.where(dx>60)[0]+1, np.nan)
+
+    return [x, y]
+
+
+records_map = {}
 for name in sys.argv[1:]:
-    hrr += heart_rate.get_heart_rate(FitFile(name))
+    #type = name.split('/')[0]
+    f = FitFile(name)
+    type = heart_rate.get_file_type(f)
+    if type == heart_rate.file_type_activity and heart_rate.is_tracking_activity(f):
+        type = heart_rate.file_type_tracking
 
-hrr.sort(key=lambda x: x['timestamp'])
+    record = heart_rate.get_heart_rate(f)
+    print(type + " - " + name)
 
-data = []
-for obj in hrr:
-    data.append([obj['timestamp'], obj['heart_rate']])
+    if type not in records_map:
+        records_map[type] = []
+    records_map[type].extend(record)
 
-data = np.array(data)
-ts = data[:,0]
-bpm = data[:,1]
-
-# Filter large breaks in data continuity
-dx = ts[1:] - ts[:-1]
-x = np.insert(ts, np.where(dx>60)[0]+1, np.nan)
-x = np.asarray(x, dtype='datetime64[s]')
-x = list(map(lambda a: a - np.timedelta64(8, 'h'), x))  # UTC to EST
-y = np.insert(bpm, np.where(dx>60)[0]+1, np.nan)
+plot_data = {}
+for type, records in records_map.items():
+    records.sort(key=lambda x: x['timestamp'])
+    if type not in plot_data:
+        plot_data[type] = []
+    for record in records:
+        plot_data[type].append([record['timestamp'], record['heart_rate']])
 
 fig, ax = plt.subplots()
-ax.plot(x, y, 'bo-', markersize=2)
-ax.xaxis.set_major_locator(mdates.HourLocator(interval=1))
-ax.xaxis.set_minor_locator(mdates.MinuteLocator(interval=15))
+for type, data in plot_data.items():
+    plot_data[type] = clean_data(data)
+    ax.plot(plot_data[type][0], plot_data[type][1], 'o-', markersize=2)
+
+ax.xaxis.set_major_locator(mdates.HourLocator(interval=2))
+ax.xaxis.set_minor_locator(mdates.MinuteLocator(interval=30))
 ax.xaxis.set_major_formatter(mdates.DateFormatter('%H:%m'))
 ax.grid(True)
-
-print(x[0])
-print(x[-1])
 
 #plt.plot(x, y)
 plt.show()
