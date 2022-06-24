cda236f1c4ff92f3a015951c441804e03267b785
Connor Demille <subtlepseudonym@gmail.com>
2022-06-24 18:59:28 -0400

    Import monitor files and update logging

--- fit-import.sh
+++ fit-import.sh
@@ -15,26 +15,35 @@ while ! mountpoint --quiet "${GARMIN_MOUNT_PATH}"; do
 done
 
 if [[ 0 >= "$(ls "${GARMIN_MOUNT_PATH}" | wc -l)" ]]; then
-	echo "no files to import"
+	echo "no files on mount path"
 	exit 0
 fi
 
-/usr/bin/rsync \
-	--stats \
-	--info=name1 \
-	--no-perms \
-	--chmod=-x \
-	--owner \
-	--group \
-	--chown=1000:1000 \
-	${GARMIN_MOUNT_PATH}/GARMIN/Activity/*.fit \
-	"${DESTINATION_PATH}/activity"
+import () {
+	echo "importing $(basename $2)"
+	/usr/bin/rsync \
+		--stats \
+		--info=name1 \
+		--no-perms \
+		--chmod=-x \
+		--owner \
+		--group \
+		--chown=1000:1000 \
+		"$1" \
+		"$2"
 
-if [ $? != 0 ]; then
-	>&2 echo "rsync failed"
-	exit 1
-fi
+	if [ $? != 0 ]; then
+		>&2 echo "rsync failed"
+		exit 1
+	fi
+	echo
+}
 
+# import fit files
+import ${GARMIN_MOUNT_PATH}/GARMIN/Activity/*.fit "${DESTINATION_PATH}/activity"
+import ${GARMIN_MOUNT_PATH}/GARMIN/Monitor/*.FIT "${DESTINATION_PATH}/monitor"
+
+# remove tracking files from device
 for f in $(find "${GARMIN_MOUNT_PATH}/GARMIN/Activity/" -type f -name '*.fit'); do
 	type="$(fit_type --data ${f})"
 	if [[ "${type}" == "track" ]]; then
