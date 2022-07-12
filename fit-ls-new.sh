f6edc222f30de2366a03070efa16dcc0c0825ef3
Connor Demille <subtlepseudonym@gmail.com>
2022-07-12 00:59:17 -0400

    Use long form of --date flag

--- fit-ls-new.sh
+++ fit-ls-new.sh
@@ -7,9 +7,9 @@ fi
 SOURCE="${1}"
 SINCE="${2}"
 
-since_unix="$(date -d "${SINCE}" +%s)"
+since_unix="$(date --date "${SINCE}" +%s)"
 for f in $(find "${SOURCE}" -type f -name '*.fit' | sort); do
-	file_date="$(date -d "$(basename "${f}" | cut -d- -f1,2,3)" +%s)"
+	file_date="$(date --date "$(basename "${f}" | cut -d- -f1,2,3)" +%s)"
 	if [[ "${since_unix}" -gt "${file_date}" ]]; then
 		continue
 	fi
